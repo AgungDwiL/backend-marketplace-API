9965f1bfb9186790e27be847297200a9e574a0f2
Agung Dwi Laksana <agung.laksana16gmail.com>
2026-03-08 04:07:17 +0700

    create login api route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function login()
+    {
+        # code...
+    }
+
+    public function register()
+    {
+        # code...
+    }
+
+    public function logout()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ Route::patterns([
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::patch('/{id}', [UserController::class, 'update']);
+});
 
 Route::group(['prefix' => 'product'], function () {
     Route::get('/', [ProductController::class, 'index']);
