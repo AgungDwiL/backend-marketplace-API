f84550d105c18924fb2207fe11c3d860742ef21f
Agung Dwi Laksana <agung.laksana16gmail.com>
2026-03-07 20:43:58 +0700

    create api routes

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        # code...
+    }
+
+    public function indexByUser()
+    {
+        # code...
+    }
+
+    public function detail()
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function delete()
+    {
+        # code...
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::patterns([
+    'id' => '[0-9]+',
+    'user_id' => '[0-9]+',
+]);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'product'], function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{user_id}', [ProductController::class, 'indexByUser']);
+    Route::get('/{id}', [ProductController::class, 'detail']);
+    Route::post('/', [ProductController::class, 'create']);
+    Route::patch('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'delete']);
+});
+
+Route::group(['prefix' => 'checkout'], function () {
+    Route::get('/', [CheckoutController::class, 'index']);
+    Route::get('/{id}', [CheckoutController::class, 'detail']);
+    Route::post('/', [CheckoutController::class, 'create']);
+    Route::patch('/{id}', [CheckoutController::class, 'update']);
+    Route::delete('/{id}', [CheckoutController::class, 'delete']);
+});
+
+Route::group(['prefix' => 'transaction'], function () {
+    Route::get('/', [TransactionController::class, 'index']);
+    Route::get('/{id}', [TransactionController::class, 'detail']);
+    Route::post('/', [TransactionController::class, 'create']);
+    Route::delete('/{id}', [TransactionController::class, 'delete']);
+});
